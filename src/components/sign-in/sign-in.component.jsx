7b7f0a0189a2850902ae4ee352636061032472dd
IdrisIsra <israpilov.idris@gmail.com>
2020-07-19 21:43:29 +0300

    added reduce, header and app got userReducers and userActions

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -47,7 +47,7 @@ class SignIn extends React.Component{
                 <FormInput handleChange={this.handleChange} name="password" type="password" value={this.state.password} label="Password" required />
                 <div className="buttons">
                     <CustomButton type="submit">Sign in</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                    <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
                 </div>
             </form>
         </div>)
